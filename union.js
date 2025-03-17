cbf26d7e00ecdb8389c6e244f26d68d1ee3725c2
Kanchan Kalauni <kanchankalauni03@gmail.com>
2025-03-17 20:43:23 +0530

    union assignment

---
+++ union.js
@@ -0,0 +1,14 @@
+// function union(arr1, arr2) {
+//     let newArr = []
+//     let combineArr = [...arr1, ...arr2]
+//     for (let i = 0; i < combineArr.length; i++) {
+//         if (!newArr.includes(combineArr[i])) {
+//             newArr.push(combineArr[i])
+//         }
+//     }
+//     return newArr
+// }
+
+
+
+module.exports = union;
